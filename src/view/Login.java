82ca9946d86ff06d3e56ea4dfc6d576b8ffb6938
JhoanDev <jhoandev.log@gmail.com>
2024-03-17 10:37:31 -0300

    feat: login

--- src/view/Login.java
+++ src/view/Login.java
@@ -1,6 +1,30 @@
 package view;
 
+import java.util.Scanner;
+
 public class Login {
 
-    
+    public static void login(Scanner scanner) {
+        boolean loggedIn = false;
+        while (!loggedIn) {
+            try {
+                System.out.print("Insira seu login: ");
+                String login = scanner.nextLine();
+                System.out.print("Insira sua senha: ");
+                String senha = scanner.nextLine();
+                if (login.equals("admin") && senha.equals("admin")) {
+                    System.out.println("Login bem-sucedido!");
+                    loggedIn = true;
+                } else {
+                    throw new Exception("Login ou senha incorretos.");
+                }
+            } catch (Exception e) {
+                System.out.println("Erro: " + e.getMessage());
+                System.out.println("Tente novamente.");
+            }
+        }
+
+        // Fecha o scanner quando não for mais necessário
+        scanner.close();
+    }
 }
