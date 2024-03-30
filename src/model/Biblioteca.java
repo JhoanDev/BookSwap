9d61cfa21b2ae5d8417e59dfd0bce23f58efb45b
JhoanDev <jhoandev.log@gmail.com>
2024-03-30 10:39:00 -0300

    feat: biblioteca e livros

--- src/model/Biblioteca.java
+++ src/model/Biblioteca.java
@@ -1,5 +1,45 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Biblioteca {
+    private ArrayList<Livro> livros;
+    private Usuario usuario;
+    private int quantidadeLivros;
+
+    public Biblioteca(Usuario usuario) {
+        this.usuario = usuario;
+        this.livros = new ArrayList<>();
+        this.quantidadeLivros = 0;
+    }
+
+    public void adicionarLivro(Livro livro) {
+        this.livros.add(livro);
+        this.quantidadeLivros++;
+    }
+
+    public void removerLivro(Livro livro) {
+        this.livros.remove(livro);
+        this.quantidadeLivros--;
+    }
+
+    public ArrayList<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(ArrayList<Livro> livros) {
+        this.livros = livros;
+    }
     
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public int getQuantidadeLivros() {
+        return quantidadeLivros;
+    }
 }
--- src/model/Livro.java
+++ src/model/Livro.java
@@ -1,5 +1,62 @@
 package model;
 
 public class Livro {
+    private String titulo;
+    private String autor;
+    private String editora;
+    private int ano;
+    private int edicao;
+
+    public Livro(String titulo, String autor, String editora, int ano, int edicao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editora = editora;
+        this.ano = ano;
+        this.edicao = edicao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public int getEdicao() {
+        return edicao;
+    }
+
+    public void setEdicao(int edicao) {
+        this.edicao = edicao;
+    }
     
+    @Override
+    public String toString() {
+        return "Livro{" + "titulo=" + titulo + ", autor=" + autor + ", editora=" + editora + ", ano=" + ano + ", edicao=" + edicao + '}';
+    }
 }
