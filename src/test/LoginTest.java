9128dfd8b465b6a2c4604f797ab1ba6250ea4bb8
dayvisonmsilva <dayvisoneryc@gmail.com>
2024-04-15 00:05:37 -0300

    Teste de Login realizado e modificações em Login.java

---
+++ src/test/LoginTest.java
@@ -0,0 +1,57 @@
+package test;
+
+// import model.Usuario;
+import controller.Login;
+import model.UsuariosRepo;
+import model.InstituicoesRepo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+import static org.junit.jupiter.api.Assertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+
+public class LoginTest {
+
+    private UsuariosRepo usuariosRepo;
+
+    @BeforeEach
+    public void setUp() {
+        UsuariosRepo ur = UsuariosRepo.getInstance();
+        InstituicoesRepo instituicoesRepo = InstituicoesRepo.getInstance();
+    
+        InstituicoesRepo.cadastraInstituicao("UFERSA", "PAU DOS FERROS", "RN");
+        UsuariosRepo.cadastraUsuario("JHOAN", "jhoan.log", "jhoan@gmail.com", "13062004",
+                instituicoesRepo.getInstituicao("UFERSA", "PAU DOS FERROS"));
+    }
+
+    @Test
+    public void testLoginSuccess() {
+        String input = "teste\n1234\n";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        Scanner scanner = new Scanner(in);
+        assertDoesNotThrow(() -> Login.login(scanner));
+    }
+
+    @Test
+    public void testLoginFail() {
+        String input = "teste\nerrado\n";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        Scanner scanner = new Scanner(in);
+    
+        // Redirecionar a saída padrão para um OutputStream
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+    
+        Login.login(scanner);
+    
+        // Verificar se a mensagem de erro esperada foi impressa
+        assertTrue(outContent.toString().contains("Erro: Login ou senha incorretos."));
+    }
+    
+}
